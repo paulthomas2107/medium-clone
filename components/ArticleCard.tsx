70468ba8c87c36c2ed41b9df14b26b876a4fccd7
Paul Thomas <paulthomas2107@gmail.com>
2022-10-30 15:20:53 +0000

    Fix time

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -12,13 +12,24 @@ const ArticleCard: NextPage<Props> = (props) => {
     const router = useRouter();
     const { article } = props;
     
+    function getDate() {
+        let time = Date.parse(article.inserted_at);
+        let date = new Date(time);
+        return date.toLocaleDateString() + " " + date.toLocaleTimeString()
+    }
+
+
     return (
         <Card 
             isPressable 
             css={{mb: "$10"}} 
             onPress={() => router.push("/article?id=" + article.id) }
-            >
-
+        >
+            <Card.Body>
+                <Text h2>{article.title}</Text>
+                <Text b>posted {getDate()}</Text>
+                <Text b>By {article.user_email.toLowerCase()}</Text>
+            </Card.Body>        
         </Card>
     )
 }
