e132f6392d0e99b2b62770ac41fcd1941499433f
Paul Thomas <paulthomas2107@gmail.com>
2022-10-30 09:56:32 +0000

    More....

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { useState } from 'react';
+import { NextUIProvider } from '@nextui-org/react';
+import { Box } from '../components/Box';
+
+// Providers - Supabase, nextui etc
+
+// Navbar
+// Box Component for content
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      <NextUIProvider>
+        {/* Navbar */}
+        <Box css={{ px: "$12", py: "$15", mt: "$12", "@xsMax": {px: "$10"}, maxWidth: "800px", margin: "0 auto" }}>
+         <Component {...pageProps} />
+        </Box>
+      </NextUIProvider>
+    </SessionContextProvider>
+  );
 }
